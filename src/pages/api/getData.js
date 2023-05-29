69d62a7ec4062e66d79ebb1cdf2bbb4e51d00093
arafat0007 <yasinarafat0007@gmail.com>
2023-05-29 21:32:24 +0900

    backend api endpoint change, workflow addition

--- src/pages/api/getData.js
+++ src/pages/api/getData.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
       Authorization: "Bearer eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyIjp7ImlkIjoiNjQ2Y2QzMjIzMDUyNzAwMTIzMTU4MDZmIiwibmFtZSI6ImFkbWluIiwiZW1haWwiOiJhcmFmYXRAZ21haWwuY29tIn0sInN1YiI6ImFyYWZhdEBnbWFpbC5jb20iLCJpYXQiOjE2ODQ4NTM2NDIsImV4cCI6MTY4NzQ0NTY0Mn0.XkWI1q8e4JyQIXc7AfGaB7ATP0wR5w4J0P9X8QQECz43UGMauqvlKK0liiLegndgUOv8xkD_LJdr8VUbasXi3Q"
     };
 
-    const response = await fetch("http://170.64.252.188/customers?page=0&size=4&search=", {headers});
+    const response = await fetch("http://170.64.248.93/customers?page=0&size=4&search=", {headers});
     const data = await response.json();
 
     res.status(200).json(data);
